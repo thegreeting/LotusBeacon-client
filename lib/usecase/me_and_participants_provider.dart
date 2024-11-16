390947cf64d40ccbfd9dd018161c5e04f01af09e
Kenichi Naoe <ken@naoe.tokyo>
2024-11-16 16:36:03 +0700

    ✨ feat(greetings): Update greeting providers to return lists of Participant objects

--- lib/usecase/me_and_participants_provider.dart
+++ lib/usecase/me_and_participants_provider.dart
@@ -1,5 +1,5 @@
-// GreetingStatus.mutual
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lotusbeacon/domain/greeting.dart';
 import 'package:lotusbeacon/domain/participant.dart';
 import 'package:lotusbeacon/usecase/auth_provider.dart';
 import 'package:lotusbeacon/usecase/greetings_provider.dart';
@@ -44,7 +44,34 @@ final meAndParticipantOnEventProvider =
   );
 });
 
-final mutualGreetingParticipantUserIdOnEventProvider = StreamProvider.family<List<String>, String>((ref, eventId) {
+final mutualGreetingParticipantOnEventProvider = StreamProvider.family<List<Participant>, String>((ref, eventId) {
+  final currentUserId = ref.watch(currentUserIdProvider);
+  if (currentUserId == null) {
+    return const Stream.empty();
+  }
+  final participantUserIds = ref.watch(participantUserIdsOnEventProvider(eventId)).asData?.value ?? [];
+
+  final participants = <Participant>[];
+
+  for (final participantUserId in participantUserIds) {
+    final participant = ref.watch(meAndParticipantOnEventProvider(
+      (
+        eventId: eventId,
+        participantUserId: participantUserId,
+      ),
+    ));
+    if (participant == null) {
+      throw Exception('Participant not found');
+    }
+    if (participant.greetingStatus == GreetingStatus.mutual) {
+      participants.add(participant);
+    }
+  }
+
+  return Stream.value(participants);
+});
+
+final sentGreetingParticipantUserIdOnEventProvider = StreamProvider.family<List<Participant>, String>((ref, eventId) {
   final currentUserId = ref.watch(currentUserIdProvider);
   if (currentUserId == null) {
     return const Stream.empty();
@@ -54,17 +81,8 @@ final mutualGreetingParticipantUserIdOnEventProvider = StreamProvider.family<Lis
   return const Stream.empty();
 });
 
-final sentGreetingParticipantUserIdOnEventProvider = StreamProvider.family<List<String>, String>((ref, eventId) {
-  final currentUserId = ref.watch(currentUserIdProvider);
-  if (currentUserId == null) {
-    return const Stream.empty();
-  }
-  final participantUserIds = ref.watch(participantUserIdsOnEventProvider(eventId));
-  // TODO(knaoe): To be impl.
-  return const Stream.empty();
-});
-
-final receivedGreetingParticipantUserIdOnEventProvider = StreamProvider.family<List<String>, String>((ref, eventId) {
+final receivedGreetingParticipantUserIdOnEventProvider =
+    StreamProvider.family<List<Participant>, String>((ref, eventId) {
   final currentUserId = ref.watch(currentUserIdProvider);
   if (currentUserId == null) {
     return const Stream.empty();
@@ -75,7 +93,7 @@ final receivedGreetingParticipantUserIdOnEventProvider = StreamProvider.family<L
 });
 
 final noneGreetingButNearByParticipantUserIdOnEventProvider =
-    StreamProvider.family<List<String>, String>((ref, eventId) {
+    StreamProvider.family<List<Participant>, String>((ref, eventId) {
   final currentUserId = ref.watch(currentUserIdProvider);
   if (currentUserId == null) {
     return const Stream.empty();
@@ -85,7 +103,7 @@ final noneGreetingButNearByParticipantUserIdOnEventProvider =
   return const Stream.empty();
 });
 
-final noneGreetingParticipantUserIdOnEventProvider = StreamProvider.family<List<String>, String>((ref, eventId) {
+final noneGreetingParticipantUserIdOnEventProvider = StreamProvider.family<List<Participant>, String>((ref, eventId) {
   final currentUserId = ref.watch(currentUserIdProvider);
   if (currentUserId == null) {
     return const Stream.empty();
