6696759dab2ff8ab76ae55f3a3b0d37501e1ede8
Kenichi Naoe <ken@naoe.tokyo>
2024-11-16 11:14:18 +0700

    🔧 refactor: Simplify proximity stream provider and remove unused imports

--- lib/usecase/bluetooth_provider.dart
+++ lib/usecase/bluetooth_provider.dart
@@ -1,15 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:rxdart/rxdart.dart';
-import 'package:uuid/uuid.dart';
 
 import '../domain/physical_proximity.dart';
 import '../driver/bluetooth/bluetooth_proximity_service.dart';
 
 final bleServiceProvider = Provider((ref) => BleProximityService());
 
-final rpidProvider = Provider((ref) => const Uuid().v4());
-
 final proximityStreamProvider = StreamProvider<List<LotusBeaconPhysicalHandshake>>((ref) {
   final service = ref.watch(bleServiceProvider);
-  return service.proximityStream.map((proximity) => [proximity]).startWith([]);
+  return service.proximityDataStream;
 });
